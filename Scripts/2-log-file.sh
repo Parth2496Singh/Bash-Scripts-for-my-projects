ef39df9c8ca2fba2368125d172e0d104958ac947
Parth2496Singh <parthsinghkushwaha24@gmail.com>
2026-06-20 16:03:17 +0530

    Added Bash Scripts for automated deployments for GiftHaven and Mern-Ecommerce

--- Scripts/2-log-file.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-LOG_FILE="/home/ubuntu/Linux-Automation-Toolkit/Logs/Automation.log"
-
-echo "$(date) - Script Executed Successfully " >> $LOG_FILE
-echo "Hello, Script ran successfully" 
